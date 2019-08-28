39f14c1d250b211db34ae0cee89c4fb2ad055ef6
borysovdenys <33123785+borysovdenys@users.noreply.github.com>
2019-08-29 00:31:12 +0300

    first commit

---
+++ src/main/java/borysov/khpi/diploma/RunEdgeDetection.java
@@ -0,0 +1,15 @@
+package borysov.khpi.diploma;
+
+import borysov.khpi.diploma.ui.EdgeDetectionUI;
+
+import java.io.IOException;
+
+/**
+ * Created by Denys Borysov
+ */
+public class RunEdgeDetection {
+
+    public static void main(String[] args) throws IOException {
+        new EdgeDetectionUI();
+    }
+}
